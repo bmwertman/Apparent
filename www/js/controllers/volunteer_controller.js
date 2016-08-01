79009846607b564b00d0c0ffec8056c39a952f9a
Brad Wertman <brad.wertman@leanmeanz.com>
2016-08-01 00:00:45 -0400

    Adds ability to create a group chat room with all currently signed up volunteers of an event that will then be associated with that event by the event id

--- www/js/controllers/volunteer_controller.js
+++ www/js/controllers/volunteer_controller.js
@@ -2,21 +2,43 @@ Pta.controller('VolunteerCtrl', [
     '$scope',
     '$state',
     'Rooms',
-    function ($scope, $state, Rooms) {
-
+    'userService',
+    function ($scope, $state, Rooms, userService) {
+    var   chatWarp = {
+            path:{ 
+                radius: 14,
+                angle: "0deg" 
+            },
+            targets:"#chatwarp",
+            align: "center",
+        },
+        allWarp = {
+            path:{ 
+                radius: 28,
+                angle: "180deg",
+                textPosition: "inside" 
+            },
+            targets:"#allwarp",
+            align: "center",
+        };
+    $scope.user = userService.getUser();
     $scope.thisHoursVolunteers = $state.params.thisHoursVolunteers;
     $scope.thisEvent = $state.params.thisEvent;
+
+    cssWarp(chatWarp, allWarp);
 
     $scope.createRoom = function(volunteers, event, volunteer) {
         var volunteersArr = [];
         if(volunteer){
             volunteersArr.push(volunteer.user);
         } else {
-            //passing all who are currently volunteered
+            for (var i = volunteers.length - 1; i >= 0; i--) {
+                if(volunteers[i].user.$id !== $scope.user.$id){
+                    volunteersArr.push(volunteers[i].user);
+                }
+            }
         }
-        
         var id = Rooms.addNewRoom(volunteersArr, '/event-rooms/', event);
         $state.go('app.rooms.chat', {roomId: id});
     }
-
 }]);    
