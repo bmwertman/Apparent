a07ecbba42c84b34bc8d753c433377e3e3b8a7a7
Brad Wertman <brad.wertman@leanmeanz.com>
2016-08-10 16:42:48 -0400

    adds user select dropdow for choosing a user to fill a role

--- www/js/app.js
+++ www/js/app.js
@@ -14,6 +14,7 @@ var Pta = angular.module('pta', [
   'naif.base64',
   'firebase',
   'angularMoment',
+  'selector',
   angularDragula(angular)
   ])
 .run(function($ionicPlatform, $rootScope, Auth, editableThemes, editableOptions, $localstorage, $firebaseAuth) {
--- www/js/controllers/role_controller.js
+++ www/js/controllers/role_controller.js
@@ -3,12 +3,14 @@ Pta.controller('RoleCtrl', [
   'userService',
   '$ionicPopup',
   '$firebaseArray',
-  '$timeout',
-  function ($scope, userService, $ionicPopup, $firebaseArray, $timeout) {
+  'userFilter',
+  function ($scope, userService, $ionicPopup, $firebaseArray, userFilter) {
   $scope.user = userService.getUser();  
   
   var ref = firebase.database().ref(),
       rolesRef = ref.child('roles/' + $scope.user.school),
+      users = firebase.database().ref('users'),
+      parents = $firebaseArray(users.orderByChild('school').equalTo($scope.user.school)),
       adminWarp = {
         path:{ 
             radius: 24,
@@ -21,19 +23,16 @@ Pta.controller('RoleCtrl', [
       };
   cssWarp(adminWarp);
 
+  parents.$loaded()
+  .then(function(schoolParents){
+      $scope.schoolParents = schoolParents;
+  });
+  $scope.showOptions = false;
   $scope.roles = $firebaseArray(rolesRef);
 
   $scope.addRole = function(){
-    rolesRef.push({title: "", user: ""});
+    rolesRef.push({title: "", user_name: "", user_id: ""});
   }
-
-  $scope.roleAssignees = document.getElementsByClassName('role-assignees');
-  
-  $timeout(function(){
-    $scope.$watchGroup('roleAssignees', function(newValues, oldValues, $scope){
-      debugger;
-    });
-  });
 
   $scope.removeRole = function(role){
     if(role.user && role.title){
@@ -60,27 +59,39 @@ Pta.controller('RoleCtrl', [
       $scope.roles.$remove(role);
     } 
   }
-
-  // $scope.confirmChange = function(role){
-  //   var popupTitle,
-  //       popupTemplate,
-  //       confirmPopup = $ionicPopup.confirm({
-  //         title: popupTitle,
-  //         template: popupTemplate,
-  //         okText: "Save",
-  //         okType: "button-balanced"
-  //       });
-  //   if(role.user.length > 0 && role.title.length > 0){
-  //     popupTitle = "You are replacing " + role.user + " as" + role.title;
-  //     popupTemplate = "Are you sure you want to replace" + role.user + " with " + $scope.role.user + "?";
-  //     confirmPopup.then(function(res){
-  //       if(res){
-  //         $scope.roles.$save(role);
-  //       }
-  //     });
-  //   } else {
-  //     $scope.roles.$save(role)
-  //   }
-  // }
+  
+  $scope.confirmChange = function(user, role){
+    var newAdminRef = 'users/' + user.$id + '/isAdmin',
+        updates = {};
+    updates[newAdminRef] = true;
+    if(role.user_id && role.user_id.length > 0){
+      var oldAdminRef = 'users/' + role.user_id + '/isAdmin',
+          confirmPopup = $ionicPopup.confirm({
+            title: "You are replacing " + role.user_name + " as " + role.title,
+            template: "Are you sure you want to replace " + role.user_name + " with " + user.name + "?",
+            okText: "Save",
+            okType: "button-balanced"
+          });
+  
+      confirmPopup.then(function(res){
+        if(res){
+          updates[oldAdminRef] = false;
+          ref.update(updates);
+          rolesRef.child(role.$id).update({
+            user_name: user.name,
+            user_id: user.$id,
+            user_pic: user.pic
+          });
+        }
+      });
+    } else {
+      ref.update(updates);
+      rolesRef.child(role.$id).update({
+        user_name: user.name,
+        user_id: user.$id,
+        user_pic: user.pic
+      });
+    }
+  }
 
 }]);
