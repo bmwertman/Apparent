51c0b0011f6929b6da2aea7699e6b8e068f58e64
Brad Wertman <brad.wertman@leanmeanz.com>
2016-10-17 11:24:10 -0400

    fixes issue with view redirecting to the landing view upon adding a cropped image to the new event submit form. See email on Oct 17 2016 or reference firebase issue number 9-9849000014051

--- www/js/factories/imgStorage_factory.js
+++ www/js/factories/imgStorage_factory.js
@@ -16,92 +16,46 @@ Pta.factory('imgStorage',[
         array[i] = binary.charCodeAt(i) 
       }
       var image = new Blob([array]);
-      uploadTask.getDownloadURL()
-      .then(function(url){// Ensure we're only storing one logo per school
-        uploadTask.delete()
-        .then(function(){
-          var uploadTask = storageRef.child(storagePath).put(image);
-          uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, {
-            next: function(snapshot){
-              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              switch (snapshot.state) {
-                case firebase.storage.TaskState.PAUSED: // or 'paused'
-                  console.log('Upload is paused');
-                  break;
-                case firebase.storage.TaskState.RUNNING: // or 'running'
-                  console.log('Uploading ' + progress + "% complete");
-                  break;
-              }
-            }, 
-            error: function(error){
-              switch (error.code) {
-                 case 'storage/unauthorized':
-                   console.log("You don't have permission to access this image");
-                   break;
+      var uploadTask = storageRef.child(storagePath).put(image);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, {
+        next: function(snapshot){
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Uploading ' + progress + "% complete");
+              break;
+          }
+        }, 
+        error: function(error){
+          switch (error.code) {
+             case 'storage/unauthorized':
+               console.log("You don't have permission to access this image");
+               break;
 
-                 case 'storage/canceled':
-                   console.log("Upload was canceled");
-                   break;
-              }
-            },
-            complete: function(){
-              if(imgUse && imgUse === 'logo'){
-                  school.logo = uploadTask.snapshot.downloadURL;
-                  school.$save().then(function(){
-                    cb();
-                  });
-              } else {
-                storageRef.child(storagePath).getDownloadURL()
-                .then(function(imgUrl){
-                  cb(imgUrl); 
-                })
-                .catch(function(err){
-                  console.log("Error: ", err);
-                });  
-              }
-            }
-          });
-        }).catch(function(error){
-          console.log(error);
-        });
-      }).catch(function(onReject){
-        var uploadTask = storageRef.child(storagePath).put(image);
-        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, {
-          next: function(snapshot){
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            switch (snapshot.state) {
-              case firebase.storage.TaskState.PAUSED: // or 'paused'
-                break;
-              case firebase.storage.TaskState.RUNNING: // or 'running'
-                break;
-            }
-          }, 
-          error: function(error){
-            switch (error.code) {
-               case 'storage/unauthorized':
-                 break;
-               case 'storage/canceled':
-                 console.log("Upload was canceled");
-                 break;
-            }
-          },
-          complete: function(){
-            if(imgUse && imgUse === 'logo'){
+             case 'storage/canceled':
+               console.log("Upload was canceled");
+               break;
+          }
+        },
+        complete: function(){
+          if(imgUse && imgUse === 'logo'){
               school.logo = uploadTask.snapshot.downloadURL;
               school.$save().then(function(){
                 cb();
               });
-            } else {
-              storageRef.child(storagePath).getDownloadURL()
-              .then(function(imgUrl){
-                cb(imgUrl); 
-              })
-              .catch(function(err){
-                console.log("Error: ", err);
-              });           
-            } 
+          } else {
+            storageRef.child(storagePath).getDownloadURL()
+            .then(function(imgUrl){
+              cb(imgUrl); 
+            })
+            .catch(function(err){
+              console.log("Error: ", err);
+            });  
           }
-        });
+        }
       });
     }
   }
